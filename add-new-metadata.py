b4779857ffef5e9b6ae649e0d9a36fba505b895b
Jake Kaufman <me@jake.computer>
2024-06-18 13:15:50 -0400

    Update repo
    * Update pre-commit
    * pull forward updates to python310+
    * commit to "" in python :(

--- add-new-metadata.py
+++ add-new-metadata.py
@@ -4,28 +4,25 @@ import json
 import os
 import subprocess
 import sys
-from typing import List
-from typing import Optional
-from typing import Set
 
 METADATA_TEMPLATE = {
-    'alt': '',
-    'camera': '',
-    'date': '',
-    'film': '',
-    'subtitle': '',
+    "alt": "",
+    "camera": "",
+    "date": "",
+    "film": "",
+    "subtitle": "",
 }
 
 
 def edit_json(json_name: str) -> int:
-    inp = input('s?')
-    if inp == 's':
+    inp = input("s?")
+    if inp == "s":
         return 0
-    return subprocess.call(['vim', json_name])
+    return subprocess.call(["vim", json_name])
 
 
 def create_empty_metadata(json_name: str) -> bool:
-    with open(json_name, 'w') as f:
+    with open(json_name, "w") as f:
         json.dump(
             METADATA_TEMPLATE,
             f,
@@ -40,19 +37,19 @@ def update(
     image_name: str,
     json_name: str,
     always_edit: bool,
-    fields: Optional[Set[str]],
+    fields: set[str] | None,
 ) -> int:
     if not os.path.exists(json_name):
-        print('Creating missing {json_name}')
+        print("Creating missing {json_name}")
         create_empty_metadata(json_name)
     try:
         with open(json_name) as f:
             metadata = json.load(f)
     except FileNotFoundError as e:
-        print(f'Unable to load metadata file: {json_name}.', e)
+        print(f"Unable to load metadata file: {json_name}.", e)
         return 1
     except json.decoder.JSONDecodeError as e:
-        print(f'Unable to parse metadata file: {json_name}.', e)
+        print(f"Unable to parse metadata file: {json_name}.", e)
         return edit_json(json_name)
 
     edit = always_edit
@@ -61,7 +58,7 @@ def update(
             break
         if fields and k not in fields:
             continue
-        if v == '':
+        if v == "":
             edit = True
 
     if edit:
@@ -72,49 +69,49 @@ def update(
             indent=2,
         )
         print()
-        print(f'editing {os.path.basename(image_name)}')
+        print(f"editing {os.path.basename(image_name)}")
         return edit_json(json_name)
     else:
-        print(f'No need to edit {json_name}')
+        print(f"No need to edit {json_name}")
         return 0
 
 
-def main(argv: Optional[List[str]] = None) -> int:
+def main(argv: list[str] | None = None) -> int:
     parser = argparse.ArgumentParser(
-        description='copy and create metadata files for new images',
+        description="copy and create metadata files for new images",
     )
     parser.add_argument(
-        '--config-file',
-        help='Path to config file. defaults to ./config.json',
-        default='config.json',
+        "--config-file",
+        help="Path to config file. defaults to ./config.json",
+        default="config.json",
     )
     parser.add_argument(
-        '--always-edit',
-        help='Comma separate list of fields to check if a file needs editing.',
+        "--always-edit",
+        help="Comma separate list of fields to check if a file needs editing.",
         action=argparse.BooleanOptionalAction,
         default=False,
     )
     parser.add_argument(
-        '--fields',
-        help='Comma separate list of fields to check if a file needs editing.',
+        "--fields",
+        help="Comma separate list of fields to check if a file needs editing.",
         default=None,
     )
     parser.add_argument(
-        'source_dir',
-        help='Source directory for images',
+        "source_dir",
+        help="Source directory for images",
     )
     args = parser.parse_args(argv)
-    fields = args.fields.split(',') if args.fields else None
+    fields = args.fields.split(",") if args.fields else None
     OUTPUT_DIR = args.source_dir
-    IMAGE_DIR = os.path.join(OUTPUT_DIR, 'images')
-    METADATA_DIR = os.path.join(OUTPUT_DIR, 'metadata')
+    IMAGE_DIR = os.path.join(OUTPUT_DIR, "images")
+    METADATA_DIR = os.path.join(OUTPUT_DIR, "metadata")
     rets = 0
     for file in os.listdir(IMAGE_DIR):
         prefix, ext = os.path.splitext(file)
-        if ext == '.jpg':
+        if ext == ".jpg":
             rets += update(
                 os.path.join(IMAGE_DIR, file),
-                os.path.join(METADATA_DIR, prefix + os.path.extsep + 'json'),
+                os.path.join(METADATA_DIR, prefix + os.path.extsep + "json"),
                 args.always_edit,
                 fields,
             )
@@ -122,5 +119,5 @@ def main(argv: Optional[List[str]] = None) -> int:
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(main())
--- generate-new-images.py
+++ generate-new-images.py
@@ -3,38 +3,35 @@ import argparse
 import json
 import os
 import shutil
-from typing import List
-from typing import Optional
-from typing import Tuple
 
 METADATA_TEMPLATE = {
-    'alt': '',
-    'camera': '',
-    'date': '',
-    'film': '',
-    'subtitle': '',
+    "alt": "",
+    "camera": "",
+    "date": "",
+    "film": "",
+    "subtitle": "",
 }
 
-OUTPUT_DIR = 'queued'
-IMAGE_DIR = os.path.join(OUTPUT_DIR, 'images')
-METADATA_DIR = os.path.join(OUTPUT_DIR, 'metadata')
+OUTPUT_DIR = "queued"
+IMAGE_DIR = os.path.join(OUTPUT_DIR, "images")
+METADATA_DIR = os.path.join(OUTPUT_DIR, "metadata")
 
 
-def unused(dates: List[Tuple[str, str]], new_image: str) -> bool:
+def unused(dates: list[tuple[str, str]], new_image: str) -> bool:
     for date, image in dates:
         if image == new_image:
-            print(f'{image} is already used on {date}')
+            print(f"{image} is already used on {date}")
             return False
         if os.path.exists(os.path.join(IMAGE_DIR, new_image)):
-            print(f'{new_image} is already in {IMAGE_DIR}')
+            print(f"{new_image} is already in {IMAGE_DIR}")
             return False
     return True
 
 
 def write_metadata(json_name: str) -> int:
-    print(f'Creating {json_name}')
+    print(f"Creating {json_name}")
     try:
-        with open(json_name, 'w') as f:
+        with open(json_name, "w") as f:
             json.dump(
                 METADATA_TEMPLATE,
                 f,
@@ -47,7 +44,7 @@ def write_metadata(json_name: str) -> int:
         json.decoder.JSONDecodeError,
     ) as e:
         print(
-            f'Unable to write metadata: {json_name}.',
+            f"Unable to write metadata: {json_name}.",
             e,
         )
         return 1
@@ -55,44 +52,46 @@ def write_metadata(json_name: str) -> int:
     return 0
 
 
-def main(argv: Optional[List[str]] = None) -> int:
+def main(argv: list[str] | None = None) -> int:
     parser = argparse.ArgumentParser(
-        description='copy and create metadata files for new images',
+        description="copy and create metadata files for new images",
     )
     parser.add_argument(
-        '--config-file',
-        help='Path to config file. defaults to ./config.json',
-        default='config.json',
+        "--config-file",
+        help="Path to config file. defaults to ./config.json",
+        default="config.json",
     )
     parser.add_argument(
-        'source_dir',
-        help='Source directory for images',
+        "source_dir",
+        help="Source directory for images",
     )
     args = parser.parse_args(argv)
     try:
         with open(args.config_file) as c:
             config = json.load(c)
     except (FileNotFoundError, json.decoder.JSONDecodeError) as e:
-        print(f'Unable to load config_file: {args.config_file}.', e)
+        print(f"Unable to load config_file: {args.config_file}.", e)
         return 1
 
-    dates = config.get('dates', None)
+    dates = config.get("dates", None)
     if dates is None:
-        print('Unable to get dates from config')
+        print("Unable to get dates from config")
         return 1
 
     if not os.path.exists(args.source_dir):
-        print(f'Error: unable to list {args.source_dir}')
+        print(f"Error: unable to list {args.source_dir}")
         return 1
     with os.scandir(args.source_dir) as it:
         for entry in it:
             if entry.is_file():
                 name = entry.name
                 prefix, ext = os.path.splitext(name)
-                if ext == '.jpg' and unused(dates, name):
+                if ext == ".jpg" and unused(dates, name):
                     print(
-                        f'Moving {args.source_dir}/{name} '
-                        f'to {IMAGE_DIR}/{name}',
+                        f"Moving {
+                            args.source_dir
+                        }/{name} "
+                        f"to {IMAGE_DIR}/{name}",
                     )
                     shutil.move(
                         os.path.join(args.source_dir, name),
@@ -101,7 +100,7 @@ def main(argv: Optional[List[str]] = None) -> int:
                     ret = write_metadata(
                         os.path.join(
                             METADATA_DIR,
-                            prefix + os.path.extsep + 'json',
+                            prefix + os.path.extsep + "json",
                         ),
                     )
                     if ret != 0:
@@ -110,5 +109,5 @@ def main(argv: Optional[List[str]] = None) -> int:
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(main())
