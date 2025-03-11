576ec163dfac0fe0d6fe3f3cd72fbc0828c1b58d
Jake Kaufman <me@jake.computer>
2025-03-11 00:03:44 -0400

    DOn't do this yet

--- src/dailyphoto/exif.py
+++ src/dailyphoto/exif.py
@@ -30,7 +30,7 @@ def exif_to_metadata(image_file: str, metadata: dict[str, str]) -> None:
     exif_tags: dict[str, str | None] = {
         "Make": None,
         "Model": None,
-        "DateTimeOriginal": None,
+        "DateTime": None,
     }
 
     with Image.open(image_file) as image:
